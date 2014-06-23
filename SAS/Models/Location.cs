94aa5d1976255f0b1b856d009b323f1d4616142b
al <xingyong.xie@estay.cn>
2014-06-23 16:48:02 +0800

    add the interface of retuning the position

---
+++ SAS/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAS.Models
+{
+    public class Location
+    {
+        public object id;
+        public object name;
+        public Location(object id, object name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
