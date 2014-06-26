28a4ec634edbbeddb9d9c7cf7de18474b1314de1
itiwll <itiwll@gmail.com>
2014-06-26 09:49:35 +0800

    input_tip更新；

--- SAS/itiwll_tool/src/script/plug-in/input_tip.js
+++ SAS/itiwll_tool/src/script/plug-in/input_tip.js
@@ -6,13 +6,30 @@
 (function($) {
 	$.fn.e_input_tip = function(options) {
 		var defaults = "请输入";
-			return this.each(function() {
-				var el = $(this);
-				var text = options ? options : defaults,
-					attr_data = $(this).attr("tip_text");
-				text = attr_data ? attr_data : text;
-				el.addClass('col_gray');
-				el.val(text);
+		return this.each(function() {
+			var el = $(this);
+			var text = options ? options : defaults,
+				tip_text = $(this).attr("tip_text");
+			text = tip_text ? tip_text : text;
+			el.addClass('col_gray');
+			el.val(text);
+			el.focusin(function(event) {
+				if (el.val()==text) {
+					el.val("").removeClass('col_gray');
+				};
+			})
+			.focusout(function(event) {
+				if(el.val()==""){
+					el.val(text).addClass('col_gray');
+				}
+			})
+			.keyup(function(event) {
+				if (el.val()==text) {
+					el.addClass('col_gray');
+				}else {
+					el.removeClass('col_gray');
+				};
 			});
+		});
 	}
 })(jQuery);
