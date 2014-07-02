f48cd150c38c1e0895c9e834890e263da87a2ac3
al <xingyong.xie@estay.cn>
2014-07-02 16:33:44 +0800

    create the controller of image

---
+++ SAS/Controllers/ImageController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SAS.Models;
+
+namespace SAS.Controllers
+{
+    public class ImageController : Controller
+    {
+        private PictureDBContent db = new PictureDBContent();
+
+        //
+        // GET: /Image/
+
+        public ActionResult Index()
+        {
+            return View(db.room.ToList());
+        }
+
+        //
+        // GET: /Image/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            hotel_picture_info hotel_picture_info = db.room.Find(id);
+            if (hotel_picture_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_picture_info);
+        }
+
+        //
+        // GET: /Image/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Image/Create
+
+        [HttpPost]
+        public ActionResult Create(hotel_picture_info hotel_picture_info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.room.Add(hotel_picture_info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(hotel_picture_info);
+        }
+
+        //
+        // GET: /Image/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            hotel_picture_info hotel_picture_info = db.room.Find(id);
+            if (hotel_picture_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_picture_info);
+        }
+
+        //
+        // POST: /Image/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(hotel_picture_info hotel_picture_info)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(hotel_picture_info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hotel_picture_info);
+        }
+
+        //
+        // GET: /Image/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            hotel_picture_info hotel_picture_info = db.room.Find(id);
+            if (hotel_picture_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_picture_info);
+        }
+
+        //
+        // POST: /Image/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            hotel_picture_info hotel_picture_info = db.room.Find(id);
+            db.room.Remove(hotel_picture_info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SAS/Models/hotel_picture_info.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SAS.Models
+{
+    public class hotel_picture_info
+    {
+        public hotel_picture_info()
+        { }
+        #region Model
+        private int _h_p_id;
+        private int _hotel_id;
+        private string _h_p_title;
+        private string _h_p_pic_original_url;
+        private string _h_p_pic_thumb_url;
+        private int? _h_p_sort = 10000;
+        private DateTime _h_p_time;
+        private string _h_p_tag;
+        private int? _h_p_type;
+        private int _h_p_size;
+        /// <summary>
+        /// 酒店图片编号
+        /// </summary>
+        /// 
+         [KeyAttribute]
+        public int h_p_id
+        {
+            set { _h_p_id = value; }
+            get { return _h_p_id; }
+        }
+        /// <summary>
+        /// 酒店ID
+        /// </summary>
+        public int hotel_id
+        {
+            set { _hotel_id = value; }
+            get { return _hotel_id; }
+        }
+        /// <summary>
+        /// 图片名称
+        /// </summary>
+        public string h_p_title
+        {
+            set { _h_p_title = value; }
+            get { return _h_p_title; }
+        }
+        /// <summary>
+        /// 原图 （大）
+        /// </summary>
+        public string h_p_pic_original_url
+        {
+            set { _h_p_pic_original_url = value; }
+            get { return _h_p_pic_original_url; }
+        }
+        /// <summary>
+        /// 缩略（小）
+        /// </summary>
+        public string h_p_pic_thumb_url
+        {
+            set { _h_p_pic_thumb_url = value; }
+            get { return _h_p_pic_thumb_url; }
+        }
+        /// <summary>
+        /// 排序（默认10000 ）
+        /// </summary>
+        public int? h_p_sort
+        {
+            set { _h_p_sort = value; }
+            get { return _h_p_sort; }
+        }
+        /// <summary>
+        /// 录入时间
+        /// </summary>
+        public DateTime h_p_time
+        {
+            set { _h_p_time = value; }
+            get { return _h_p_time; }
+        }
+        /// <summary>
+        /// 关键字，(用于SEO)
+        /// </summary>
+        public string h_p_tag
+        {
+            set { _h_p_tag = value; }
+            get { return _h_p_tag; }
+        }
+        /// <summary>
+        /// 类型
+        /// </summary>
+        public int? h_p_type
+        {
+            set { _h_p_type = value; }
+            get { return _h_p_type; }
+        }
+        /// <summary>
+        /// 大小
+        /// </summary>
+        public int h_p_size
+        {
+            set { _h_p_size = value; }
+            get { return _h_p_size; }
+        }
+        #endregion Model
+    }
+    public class PictureDBContent : DbContext
+    {
+        public DbSet<hotel_picture_info> room { get; set; }
+    }
+}
