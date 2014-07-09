124f53f015c35b87d3b42702d9f12006e9fa24be
al <xingyong.xie@estay.cn>
2014-07-09 10:26:15 +0800

    p_title update

--- SAS/help/ImageDes.ashx.cs
+++ SAS/help/ImageDes.ashx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SAS.Models;
 
 namespace SAS.help
 {
@@ -10,11 +11,18 @@ namespace SAS.help
     /// </summary>
     public class ImageDes : IHttpHandler
     {
-
+        private PictureDBContent db = new PictureDBContent();
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hello World");
+            int PID=Convert.ToInt32(context.Request.Form[0]);
+            string DescText = context.Request.Form[0];
+            var p = (from i in db.room where i.h_p_id == PID select i).Single();
+            p.h_p_title = DescText;
+            if (db.SaveChanges() > 0)
+                context.Response.Write(1);
+            else
+                context.Response.Write(0);
+           
         }
 
         public bool IsReusable
