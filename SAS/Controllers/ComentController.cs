118d9593b04cd9dc2451fb8982946c2aeb5e7261
al <xingyong.xie@estay.cn>
2014-07-18 13:31:21 +0800

    add comment manager

---
+++ SAS/Controllers/ComentController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SAS.Models;
+
+namespace SAS.Controllers
+{
+    public class ComentController : Controller
+    {
+        private Hotel_comment_infoDBContent db = new Hotel_comment_infoDBContent();
+
+        //
+        // GET: /Coment/
+
+        public ActionResult Index()
+        {
+            return View(db.hotel.ToList());
+        }
+
+        //
+        // GET: /Coment/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Hotel_comment_info hotel_comment_info = db.hotel.Find(id);
+            if (hotel_comment_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_comment_info);
+        }
+
+        //
+        // GET: /Coment/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Coment/Create
+
+        [HttpPost]
+        public ActionResult Create(Hotel_comment_info hotel_comment_info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.hotel.Add(hotel_comment_info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(hotel_comment_info);
+        }
+
+        //
+        // GET: /Coment/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Hotel_comment_info hotel_comment_info = db.hotel.Find(id);
+            if (hotel_comment_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_comment_info);
+        }
+
+        //
+        // POST: /Coment/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Hotel_comment_info hotel_comment_info)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hotel_comment_info).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hotel_comment_info);
+        }
+
+        //
+        // GET: /Coment/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Hotel_comment_info hotel_comment_info = db.hotel.Find(id);
+            if (hotel_comment_info == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hotel_comment_info);
+        }
+
+        //
+        // POST: /Coment/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Hotel_comment_info hotel_comment_info = db.hotel.Find(id);
+            db.hotel.Remove(hotel_comment_info);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SAS/Models/Hotel_comment_info.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SAS.Models
+{
+    public class Hotel_comment_info
+    {
+        #region Model
+        private int _commentid;
+        private string _guid;
+        private string _orderid;
+        private int? _commentsourceid;
+        private string _content;
+        private DateTime? _createtime;
+        private string _locations;
+        private string _username;
+        private string _tel_mobile;
+        private bool _recommendtype;
+        private string _room_id;
+        private string _room_name;
+        private string _title;
+        private string _hotel_id;
+        private int _commenttypeid;
+        private int? _approve_count;
+        private bool _status;
+        private bool _isreply;
+        private string _answer;
+        private DateTime? _answer_time;
+        private int? _sign;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+         [KeyAttribute]
+        public int commentId
+        {
+            set { _commentid = value; }
+            get { return _commentid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string guid
+        {
+            set { _guid = value; }
+            get { return _guid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string orderID
+        {
+            set { _orderid = value; }
+            get { return _orderid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? commentSourceId
+        {
+            set { _commentsourceid = value; }
+            get { return _commentsourceid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string content
+        {
+            set { _content = value; }
+            get { return _content; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? createTime
+        {
+            set { _createtime = value; }
+            get { return _createtime; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string locations
+        {
+            set { _locations = value; }
+            get { return _locations; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string userName
+        {
+            set { _username = value; }
+            get { return _username; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string tel_mobile
+        {
+            set { _tel_mobile = value; }
+            get { return _tel_mobile; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool recommendType
+        {
+            set { _recommendtype = value; }
+            get { return _recommendtype; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string room_id
+        {
+            set { _room_id = value; }
+            get { return _room_id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string room_name
+        {
+            set { _room_name = value; }
+            get { return _room_name; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string title
+        {
+            set { _title = value; }
+            get { return _title; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string hotel_id
+        {
+            set { _hotel_id = value; }
+            get { return _hotel_id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int commentTypeId
+        {
+            set { _commenttypeid = value; }
+            get { return _commenttypeid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? approve_count
+        {
+            set { _approve_count = value; }
+            get { return _approve_count; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool status
+        {
+            set { _status = value; }
+            get { return _status; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsReply
+        {
+            set { _isreply = value; }
+            get { return _isreply; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string answer
+        {
+            set { _answer = value; }
+            get { return _answer; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? answer_time
+        {
+            set { _answer_time = value; }
+            get { return _answer_time; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? sign
+        {
+            set { _sign = value; }
+            get { return _sign; }
+        }
+        #endregion Model
+    }
+    public class Hotel_comment_infoDBContent : DbContext
+    {
+        public DbSet<Hotel_comment_info> hotel { get; set; }
+
+    }
+}
